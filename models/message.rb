b89b40da5419a41b0e0949f97e858c22b7cb7e75
Selvy Fitriani <selvyfiriani@gmail.com>
2021-12-09 09:24:12 +0700

    feat: update #valid for message and able to detect message with invalid conversation

--- models/message.rb
+++ models/message.rb
@@ -1,8 +1,13 @@
 class Message
   def initialize(sender:, receiver:, text:, conversation:)
+    @sender = sender
+    @receiver = receiver
+    @conversation = conversation
   end
 
   def valid?
+    return false unless @conversation.valid?
+
     true
   end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -9,12 +9,24 @@ describe Message do
         sender.save
         receiver = User.new(username: 'selvyfitriani32', name: 'Selvy')
         receiver.save
-        text = 'Hai'
         conversation = Conversation.new(first_user: sender, second_user: receiver)
+        text = 'Hai'
 
         message = Message.new(sender: sender, receiver: receiver, text: text, conversation: conversation)
 
         expect(message.valid?).to be(true)
+      end
+    end
+
+    context 'when initialize message with invalid conversation' do
+      it 'should return false' do
+        sender = User.new(username: 'selvyfitriani31', name: 'Selvy Fitriani')
+        conversation = Conversation.new(first_user: sender, second_user: nil)
+        text = 'Hai'
+
+        message = Message.new(sender: sender, receiver: nil, text: text, conversation: conversation)
+
+        expect(message.valid?).to be(false)
       end
     end
   end
