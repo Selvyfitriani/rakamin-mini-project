fb20387a7cc92a916d3660328cda2e94ef905b22
Selvy Fitriani <selvyfiriani@gmail.com>
2021-12-09 11:56:50 +0700

    refactor: update transform method to handle nil raw data

--- models/transform_helper.rb
+++ models/transform_helper.rb
@@ -2,8 +2,9 @@ require './models/user'
 
 class Transform
   def self.to_user(raw_data)
-    user = nil
+    return nil if raw_data.nil?
 
+    user = nil
     raw_data.each do |datum|
       user = User.new(datum['username'], datum['name'])
     end
@@ -12,6 +13,8 @@ class Transform
   end
 
   def self.to_id(raw_data)
+    return nil if raw_data.nil?
+    
     id = 0
     raw_data.each do |datum|
       id = datum['id'].to_i
