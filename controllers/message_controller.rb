9de9db6fad680f60061e6b1ebe58999c460009f4
Selvy Fitriani <selvyfiriani@gmail.com>
2021-12-10 12:42:43 +0700

    refactor: fix typo

--- controllers/message_controller.rb
+++ controllers/message_controller.rb
@@ -1,5 +1,6 @@
 require './models/message'
 require './models/user'
+require './models/conversation'
 
 class MessageController
   def create(params)
@@ -11,7 +12,7 @@ class MessageController
 
     sender = User.find_by_id(params['sender_id'])
     receiver = User.find_by_id(params['receiver_id'])
-    conversation = User.find_by_id(params['conversation_id'])
+    conversation = Conversation.find_by_id(params['conversation_id'])
     text = params['text']
 
     message = Message.new(sender, receiver, text, conversation)
