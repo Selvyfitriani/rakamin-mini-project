d8428b36c706010d258ae84facf95e65494217e3
Selvy Fitriani <selvyfiriani@gmail.com>
2021-12-10 16:28:34 +0700

    docs: complete sql schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE rakamin_test;
+CREATE DATABASE rakamin_db;
 
-USE rakamin_test;
+USE rakamin_db;
 
 CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT,
@@ -33,3 +33,15 @@ CREATE TABLE messages (
   FOREIGN KEY(receiver_id) REFERENCES users(id),
   FOREIGN KEY(conversation_id) REFERENCES conversations(id)
 );
+
+INSERT INTO users(id, username, name) VALUES (1, 'selvy31', 'Selvy Fitriani');
+INSERT INTO users(id, username, name) VALUES (2, 'rangga123', 'Rangga Fahreran');
+INSERT INTO users(id, username, name) VALUES (3, 'shireen10', 'Shireen');
+
+INSERT INTO conversations(id, first_user_id, second_user_id) VALUES (1, 1, 2);
+INSERT INTO conversations(id, first_user_id, second_user_id) VALUES (2, 1, 3);
+INSERT INTO conversations(id, first_user_id, second_user_id) VALUES (3, 2, 3);
+
+INSERT INTO messages(id, sender_id, receiver_id, text, conversation_id) VALUES (1, 1, 2, 'Halo Rangga', 1);
+INSERT INTO messages(id, sender_id, receiver_id, text, conversation_id) VALUES (2, 2, 1, 'Halo Selvy', 1);
+INSERT INTO messages(id, sender_id, receiver_id, text, conversation_id) VALUES (3, 2, 3, 'Semangat!', 3);
