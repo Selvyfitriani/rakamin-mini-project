5295162a266d1a22f78c5d94ed2441308a8c1b0d
Selvy Fitriani <selvyfiriani@gmail.com>
2021-12-10 14:55:06 +0700

    feat: add endpoint for #find_conversation

--- main.rb
+++ main.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
 require './controllers/user_controller'
+require './controllers/conversation_controller'
 
 post '/users/send-message' do
   controller = UserController.new
   controller.send_message(params)
+end
+
+get '/users/find-conversation' do
+  controller = ConversationController.new
+  controller.find_conversation(params)
 end
